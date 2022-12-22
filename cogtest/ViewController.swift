21a26a1ff4f34d422497434ace340d05e26eb411
mrjorgexavier <jorge.xavier@amaro.com>
2022-12-22 10:11:32 -0300

    initial commit with cognito

---
+++ cogtest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cogtest
+//
+//  Created by Jorge Xavier on 21/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
